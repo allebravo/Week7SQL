552a53c99e80323a4b82e816fde9f469b19e8c38
allebravo <alejanbravo@outlook.com>
2023-01-19 22:13:49 -0800

    adding week 8 code and file

--- mysql-java/src/main/resources/projects_schema.sql
+++ mysql-java/src/main/resources/projects_schema.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS project;
 
 CREATE TABLE project (	
-  project_id INT AUTO_INCREMENT NOT NULL,
-  project_name VARCHAR(128) NOT NULL,
-  estimated_hours DECIMAL(7, 2),
-  actual_hours DECIMAL(7, 2),
-  difficulty INT,
-  notes TEXT,
-  PRIMARY KEY (project_id)
-);
+	project_id INT AUTO_INCREMENT NOT NULL,
+	project_name VARCHAR(128) NOT NULL,
+	estimated_hours TIME,
+	actual_hours TIME,
+	difficulty INT,
+	notes TEXT,
+	PRIMARY KEY(project_id)
+    );
 
 CREATE TABLE category (
 	category_id INT AUTO_INCREMENT NOT NULL,
