f0a20b73e3c15979950e4f85ee2e1b83df582c6a
allebravo <alejanbravo@outlook.com>
2023-02-05 11:57:43 -0800

    added week 11 code

--- mysql-java/src/main/java/projects/exception/DbException.java
+++ mysql-java/src/main/java/projects/exception/DbException.java
@@ -1,33 +1,21 @@
 package projects.exception;
 
+
 @SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
-	  /**
-	   * Create an exception with a message. The message is stored in the parent class.
-	   * 
-	   * @param message The message.
-	   */
-	  public DbException(String message) {
-	    super(message);
-	  }
 
-	  /**
-	   * Create an exception with a cause. The cause is stored in the parent class.
-	   * 
-	   * @param cause The cause.
-	   */
-	  public DbException(Throwable cause) {
-	    super(cause);
-	  }
+  public DbException(String message) {
+    super(message);
+  }
 
-	  /**
-	   * Create an exception with a message and a cause.
-	   * 
-	   * @param message The message.
-	   * @param cause The cause.
-	   */
-	  public DbException(String message, Throwable cause) {
-	    super(message, cause);
-	  }
-	}
+
+  public DbException(Throwable cause) {
+    super(cause);
+  }
+
+
+  public DbException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ mysql-java/src/main/resources/projects-schema.sql
@@ -0,0 +1,90 @@
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+  project_id INT AUTO_INCREMENT NOT NULL,
+  project_name VARCHAR(128) NOT NULL,
+  estimated_hours DECIMAL(7, 2),
+  actual_hours DECIMAL(7, 2),
+  difficulty INT,
+  notes TEXT,
+  PRIMARY KEY (project_id)
+);
+
+CREATE TABLE category (
+  category_id INT AUTO_INCREMENT NOT NULL,
+  category_name VARCHAR(128) NOT NULL,
+  PRIMARY KEY (category_id)
+);
+
+CREATE TABLE step (
+  step_id INT AUTO_INCREMENT NOT NULL,
+  project_id INT NOT NULL,
+  step_text TEXT NOT NULL,
+  step_order INT NOT NULL,
+  PRIMARY KEY (step_id),
+  FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE material (
+  material_id INT AUTO_INCREMENT NOT NULL,
+  project_id INT NOT NULL,
+  material_name VARCHAR(128) NOT NULL,
+  num_required INT,
+  cost DECIMAL(7, 2),
+  PRIMARY KEY (material_id),
+  FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE project_category (
+  project_id INT NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
+  UNIQUE KEY (project_id, category_id)
+);
+
+INSERT INTO category 
+(category_name)
+VALUES ('Doors and Windows');
+
+
+SELECT * FROM project
+
+INSERT INTO project
+(project_name, estimated_hours, actual_hours, difficulty, notes)
+VALUES
+('Hang closet door', 3, 3, 2, 'Use hangers from HomeDepot');
+
+INSERT INTO material
+(project_id, material_name, num_required, cost)
+VALUES
+(1, 'Door hangers', 20, 12.95)
+
+INSERT INTO material
+(project_id, material_name, num_required, cost)
+VALUES
+(1, 'Screws', 20, 4.95)
+
+INSERT INTO step
+(project_id, step_text, step_order)
+VALUES
+(1, 'Align hangers on opening side', 1)
+
+INSERT INTO step
+(project_id, step_text, step_order)
+VALUES
+(1, 'Screw hangers into frame', 2);
+
+INSERT INTO category
+(category_id, category_name)
+VALUES
+(2, 'Repairs');
+
+INSERT INTO category
+(category_id, category_name)
+VALUES
+(3, 'Gardening');
